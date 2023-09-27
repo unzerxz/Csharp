f8320d998f845755e8cc8751d6abdbcfd09ada8f
Lucas Unzer <unzerlucas2006@gmail.com>
2023-09-27 14:56:28 -0300

    update

--- TAREFAS PROJETOS/tarefas_api/Program.cs
+++ TAREFAS PROJETOS/tarefas_api/Program.cs
@@ -6,8 +6,10 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
-builder.Services.AddCors(options =>{
-    options.AddDefaultPolicy(policy =>{
+builder.Services.AddCors(options =>
+{
+    options.AddDefaultPolicy(policy =>
+    {
         policy.AllowAnyHeader()
         .AllowAnyMethod()
         .AllowAnyOrigin();
